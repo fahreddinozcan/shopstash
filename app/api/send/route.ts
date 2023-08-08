a3d7b9066f691bc668e2c31c2b57bc52973f0eea
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-08-08 16:17:13 +0300

    qstash test

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,20 +2,31 @@ import { EmailTemplate } from "@/app/components/email-template";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// export const runtime = "edge";
-export async function GET() {
-    const resend = new Resend("re_8VuzdNyz_Ncja9WYPJNjEXdiWrY1mNoDY");
-    try {
-        resend.emails.send({
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = "re_8VuzdNyz_Ncja9WYPJNjEXdiWrY1mNoDY";
+
+export async function POST() {
+    const res = await fetch("https://api.resend.com/emails", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${RESEND_API_KEY}`,
+        },
+        body: JSON.stringify({
             from: "ShopStash <onboarding@resend.dev>",
             to: ["fahreddin@upstash.com"],
             subject: "Come back!",
             react: EmailTemplate({ firstName: "Fahreddin" }),
             text: "selam",
-        });
+        }),
+    });
 
-        return NextResponse.json("sent!");
-    } catch (error) {
-        return NextResponse.json("Error!");
+    if (res.ok) {
+        const data = await res.json();
+        return NextResponse.json(data);
     }
+
+    return NextResponse.json("ERROR!");
 }
