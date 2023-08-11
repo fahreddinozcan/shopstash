5db87f9e597d74882cfbcb80d0cbcdb35bcc5288
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-08-11 17:00:59 +0300

    test email

--- app/components/email-template.tsx
+++ app/components/email-template.tsx
@@ -41,7 +41,35 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
             );
           }
 
-          return <p key={item.id}>{item.title}</p>;
+          return (
+            <li
+              className="gap-3 w-[95%]  border-b border-neutral-300 dark:border-neutral-700 py-4 px-2 grid grid-flow-col grid-cols-min transition-all ease-in-out"
+              key={item.id}
+            >
+              <div>
+                <div className="z-30 flex  space-x-4 align-center">
+                  <div className="relative h-full w-[4.5rem] cursor-pointer overflow-hidden rounded-md border border-neutral-300 bg-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:bg-neutral-800">
+                    <img
+                      className="h-full w-full object-cover "
+                      width={68}
+                      height={68}
+                      alt={item.title}
+                      src={item.image}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className=" flex  flex-col text-base">
+                <span className="leading-tight">{item.title}</span>
+              </div>
+              <div className="grid grid-flow-row grid-rows-1">
+                <div className="flex  justify-end space-y-2 text-right text-m font-bold">
+                  ${item.price}
+                </div>
+              </div>
+            </li>
+          );
         })}
       </div>
     );
