3a5b90fa8cbe6c1de6d234bd56f9bdc83f55ffca
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-08-11 15:36:02 +0300

    test email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,12 +2,12 @@ import { EmailTemplate } from "@/app/components/email-template";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
+// export const runtime = "edge";
+// export const dynamic = "force-dynamic";
 
 const RESEND_API_KEY = "re_8VuzdNyz_Ncja9WYPJNjEXdiWrY1mNoDY";
 
-// const resend = new Resend(RESEND_API_KEY);
+const resend = new Resend(RESEND_API_KEY);
 
 type mailType =
   | "item-interest"
@@ -24,26 +24,26 @@ export async function POST(request: Request) {
   const body = await request.text();
   const { mail_type, items, user } = JSON.parse(body);
 
-  await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: "ShopStash <onboarding@resend.dev>",
-      to: [user.emailAddress],
-      subject: mailSubject[mail_type as mailType],
-      react: EmailTemplate({ user, mail_type }),
-    }),
-  });
-  //   const data = await resend.emails.send({
-  //     from: "Acme <onboarding@resend.dev>",
-  //     to: [user.emailAddress],
-  //     subject: mailSubject[mail_type as mailType],
-  //     react: EmailTemplate({ user, mail_type }),
-  //     text: "text",
+  //   await fetch("https://api.resend.com/emails", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${RESEND_API_KEY}`,
+  //     },
+  //     body: JSON.stringify({
+  //       from: "ShopStash <onboarding@resend.dev>",
+  //       to: [user.emailAddress],
+  //       subject: mailSubject[mail_type as mailType],
+  //       react: EmailTemplate({ user, mail_type }),
+  //     }),
   //   });
+  const data = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: [user.emailAddress],
+    subject: mailSubject[mail_type as mailType],
+    react: EmailTemplate({ user, mail_type }),
+    text: "text",
+  });
 
   return NextResponse.json("OK");
 }
